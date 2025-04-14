f91866ee60b778a54a080184b7d541aa65444bc2
Sameer Khan <sahibakhann989@gmail.com>
2025-04-14 21:27:30 +0300

    Update 0_ARYAN-WIFE.js

--- Priyansh/commands/0_ARYAN-WIFE.js
+++ Priyansh/commands/0_ARYAN-WIFE.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'love.jpeg');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/tVi3R6t.jpg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imghippo.com/files/Zzxa3735Ss.png", path);
 }
 
 async function makeImage({ one, two }) {
