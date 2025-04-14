b12c676b6e6f07ade45aa52a2e3ada8d07f91fc7
Sameer Khan <sahibakhann989@gmail.com>
2025-04-14 21:06:08 +0300

    Update 0_ARYAN-PAIR9.js

--- Priyansh/commands/0_ARYAN-PAIR9.js
+++ Priyansh/commands/0_ARYAN-PAIR9.js
@@ -18,7 +18,7 @@ module.exports.onLoad = async() => {
     const dirMaterial = __dirname + `/cache/canvas/`;
     const path = resolve(__dirname, 'cache/canvas', 'pairing.png9');
     if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/8IkuUPu.jpeg", path);
+    if (!existsSync(path)) await downloadFile("https://i.imghippo.com/files/UxU7438fY.jpeg", path);
 }
 
 async function makeImage({ one, two }) {
