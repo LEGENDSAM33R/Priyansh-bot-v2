327cf37c53b07c268d4d9afa26cf3242cbe90b6d
Sameer Khan <sahibakhann989@gmail.com>
2025-04-14 21:30:35 +0300

    Update 0_AADI-PURPOSE.js

--- Priyansh/commands/0_AADI-PURPOSE.js
+++ Priyansh/commands/0_AADI-PURPOSE.js
@@ -22,7 +22,7 @@ module.exports.onLoad = async() => {
   const dirMaterial = __dirname + `/cache/canvas/`;
   const path = resolve(__dirname, 'cache/canvas', 'lovep.png');
   if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-  if (!existsSync(path)) await downloadFile("https://i.imgur.com/VKvcNUA.jpg", path); 
+  if (!existsSync(path)) await downloadFile("https://i.imghippo.com/files/BZp7267UgU.jpeg", path); 
 }
 
 async function makeImage({ one, two }) {
